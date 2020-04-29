f10626ec26c0d368533452982acf3789da63072f
Alex <61654925+LAV007@users.noreply.github.com>
2020-04-29 11:25:25 +0300

    Change class Player

--- src/HomeWork_2_RockPaperScissors/Player.java
+++ src/HomeWork_2_RockPaperScissors/Player.java
@@ -1,36 +1,39 @@
 package HomeWork_2_RockPaperScissors;
 
-import java.util.Random;
-
 public class Player {
-    private VARIANTS variants;
-    private String name;
 
-    public Player(){
-        variants = VARIANTS.values()[new Random().nextInt(VARIANTS.values().length)];
-        name = "Bot";
-    }
+    private String name = "Bot";
+    private VARIANTS choice;
 
-    public Player(VARIANTS variants, String name){
-        this.variants = variants;
+
+    public Player(VARIANTS choice, String name) {
+        this.choice = choice;
         this.name = name;
     }
 
-    static public String whoWins(Player playerOne, Player playerTwo){
-        String whoWins = "";
-        if (playerOne.variants.equals(playerTwo.variants)) whoWins = "ничья";
-        else if (playerOne.variants == VARIANTS.SCISSORS && playerTwo.variants == VARIANTS.ROCK)
-            whoWins = playerTwo.name;
-        else if (playerOne.variants == VARIANTS.PAPER && playerTwo.variants == VARIANTS.ROCK)
-            whoWins = playerTwo.name;
-        else if (playerOne.variants == VARIANTS.PAPER && playerTwo.variants == VARIANTS.SCISSORS)
-            whoWins = playerTwo.name;
-        else if (playerOne.variants == VARIANTS.ROCK && playerTwo.variants == VARIANTS.SCISSORS)
-            whoWins = playerOne.name;
-        else if (playerOne.variants == VARIANTS.SCISSORS && playerOne.variants == VARIANTS.PAPER)
-            whoWins = playerOne.name;
-        else if (playerOne.variants == VARIANTS.ROCK && playerTwo.variants == VARIANTS.PAPER)
-            whoWins = playerOne.name;
-        return "Победил игрок с именем: " + whoWins;
+
+    public Player() {
+        double randomDouble = Math.random();
+        randomDouble = randomDouble * 3 + 1;
+        int randomInt = (int) randomDouble;
+        switch(randomInt) {
+            case 1: choice = VARIANTS.ROCK; break;
+            case 2: choice = VARIANTS.PAPER; break;
+            case 3: choice = VARIANTS.SCISSORS; break;
+        }
+
     }
+
+    public static String whoWins(Player firstPlayer, Player secondPlayer) {
+        if (firstPlayer.choice == secondPlayer.choice)
+            return "Ничья";
+        else if ((firstPlayer.choice == VARIANTS.ROCK && secondPlayer.choice == VARIANTS.PAPER)
+                || (firstPlayer.choice == VARIANTS.PAPER && secondPlayer.choice == VARIANTS.SCISSORS)
+                || (firstPlayer.choice == VARIANTS.SCISSORS && secondPlayer.choice == VARIANTS.ROCK))
+            return "Победил игрок с именем: " + firstPlayer.name;
+        else
+            return "Победил игрок с именем: " + secondPlayer.name;
+
+    }
+
 }
